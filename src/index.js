af003e7990cfc6925cd22d5c4fdc53401225e515
Aternus <kiril@atern.us>
2025-09-27 03:12:44 +0300

    feat(upgrade): modernized react framework

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "./index.css";
@@ -11,7 +11,9 @@ import "bootstrap/dist/js/bootstrap";
 import * as serviceWorker from "./serviceWorker";
 import App from "components/App/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
